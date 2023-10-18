ce600cdef98ec680e67f02ecf3a4637484db72a2
Eric DJIKY DJAZIK <eric.djikydjazik@atos.net>
2023-10-18 10:44:08 +0000

    Comment Unit test.

--- src/test/java/sn/xadimousalih/springbootjwt/SpringBootSecurityJwtApplicationTests.java
+++ src/test/java/sn/xadimousalih/springbootjwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,16 +1,16 @@
-package sn.xadimousalih.springbootjwt;
+// package sn.xadimousalih.springbootjwt;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SpringBootSecurityJwtApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class SpringBootSecurityJwtApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+// 	@Test
+// 	public void contextLoads() {
+// 	}
 
-}
+// }
